9105732becaaeb5e92fece2ffc20f43afcb2b1ab
Yariel12 <yarielr087@gmail.com>
2026-03-09 09:23:58 -0400

    BASEURL api new api of production

--- admin/src/apis/httpClient.js
+++ admin/src/apis/httpClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const httpClient = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: "https://proyectodeva.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
